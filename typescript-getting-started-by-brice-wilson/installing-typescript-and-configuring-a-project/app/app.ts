e6530168fd1a1aaec20208755ccd8af417438c6f
hegnemurd <hegne.murd@gmail.com>
2021-02-17 16:09:51 +0000

    Taking Advantage of Built-In Types

--- typescript-getting-started-by-brice-wilson/installing-typescript-and-configuring-a-project/app/app.ts
+++ typescript-getting-started-by-brice-wilson/installing-typescript-and-configuring-a-project/app/app.ts
@@ -1,7 +1,15 @@
 function startGame() {
   // starting a new game
-  var messagesElement = document.getElementById('messages');
-  messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';  
+
+  let playerName: string = "Audrey";
+  logPlayer(playerName);
+
+  var messagesElement = document.getElementById("messages");
+  messagesElement!.innerText = "Welcome to MultiMath! Starting new game...";
 }
 
-document.getElementById('startGame')!.addEventListener('click', startGame);
+function logPlayer(name) {
+  console.log(`New game starting for player: ${name}`);
+}
+
+document.getElementById("startGame")!.addEventListener("click", startGame);
