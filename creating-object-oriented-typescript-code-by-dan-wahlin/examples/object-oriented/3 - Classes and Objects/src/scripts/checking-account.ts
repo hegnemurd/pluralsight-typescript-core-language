1f6bab4d1a066bde4832262e7fd230b9eb805a5d
hegnemurd <hegne.murd@gmail.com>
2021-02-18 17:44:01 +0000

    Classes and Objects

--- creating-object-oriented-typescript-code-by-dan-wahlin/examples/object-oriented/3 - Classes and Objects/src/scripts/checking-account.ts
+++ creating-object-oriented-typescript-code-by-dan-wahlin/examples/object-oriented/3 - Classes and Objects/src/scripts/checking-account.ts
@@ -1,21 +1,25 @@
 ﻿export class CheckingAccount {
-    private _balance = 0; // field
+  private _balance = 0; // field
 
-    constructor(public title: string) { }
+  constructor(public title: string) {}
 
-    get balance() { // get block
-        return this._balance;
-    }
+  get balance() {
+    // get block
+    return this._balance;
+  }
 
-    set balance(val: number) { // set block
-        this._balance = val;
-    }
+  set balance(val: number) {
+    // set block
+    this._balance = val;
+  }
 
-    deposit(amount: number) { // function
-        this.balance += amount;
-    }
+  deposit(amount: number) {
+    // function
+    this.balance += amount;
+  }
 
-    withdrawal(amount: number) { // function
-        this.balance -= amount;  
-    }
+  withdrawal(amount: number) {
+    // function
+    this.balance -= amount;
+  }
 }
