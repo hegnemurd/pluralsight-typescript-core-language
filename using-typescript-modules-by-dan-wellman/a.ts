acf47077bd9110e216c3b764c522f051cce86dda
hegnemurd <hegne.murd@gmail.com>
2021-03-08 19:57:49 +0000

    Creating Basic TypeScript Modules

--- using-typescript-modules-by-dan-wellman/a.ts
+++ using-typescript-modules-by-dan-wellman/a.ts
@@ -1,6 +1,13 @@
+// export const aString = "Welcome to TypeScript";
+
 const aString = "Welcome to TypeScript";
 const bString = "Something";
 export { aString, bString };
-// export const aString = "Welcome to TypeScript";
-// export const fn = () => {}
+export const fn = () => {};
+// export defualt fn;
+
 export { message } from "./re-export";
+
+export default "default";
+
+// export = "test"; // -- can only be used with commonjs, und, amd
--- using-typescript-modules-by-dan-wellman/b.ts
+++ using-typescript-modules-by-dan-wellman/b.ts
@@ -1,3 +1,12 @@
-import { aString } from "./a";
+// import { aString as theString, bString } from "./a";
+import * as strings from "./a";
+console.log(strings.aString);
 
-console.log(aString);
+import { reverse, isOdd } from "./utils";
+console.log(reverse("test"), isOdd(1));
+
+import customStringName from "./a";
+console.log(customStringName); // default
+
+// import test = require("./a");
+// console.log(test); // test
