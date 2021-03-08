c2bde4c87995dddfeab1471689cfc6448ed6d467
hegnemurd <hegne.murd@gmail.com>
2021-03-08 19:36:27 +0000

    Creating Basic TypeScript Modules

--- using-typescript-modules-by-dan-wellman/a.ts
+++ using-typescript-modules-by-dan-wellman/a.ts
@@ -1,1 +1,6 @@
-export const aString = "Welcome to TypeScript";
+const aString = "Welcome to TypeScript";
+const bString = "Something";
+export { aString, bString };
+// export const aString = "Welcome to TypeScript";
+// export const fn = () => {}
+export { message } from "./re-export";
