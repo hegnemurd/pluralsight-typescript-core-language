46fdeadd74784ab498e1dd3ae829436bac687be1
hegnemurd <hegne.murd@gmail.com>
2021-03-09 21:26:59 +0000

    Generic Functions

--- creating-and-using-generics-in-typescript-by-brice-wilson/main.ts
+++ creating-and-using-generics-in-typescript-by-brice-wilson/main.ts
@@ -1,7 +1,16 @@
-let junkDrawer: any[] = ["cool string", 42, true];
+// let junkDrawer: any[] = ["cool string", 42, true];
 
-let companies: Array<string> = ["Microsoft", "Google", "Amazon"];
+// let companies: Array<string> = ["Microsoft", "Google", "Amazon"];
 
-let primeNums: Array<number> = [7, 11, 13];
+// let primeNums: Array<number> = [7, 11, 13];
 
-let lastValue = primeNums.pop();
+// let lastValue = primeNums.pop();
+
+function shortenArray<T>(data: Array<T>, amountToShorten: number): Array<T> {
+  return data.splice(amountToShorten, data.length);
+}
+
+let stringArray: string[] = ["Visual Basic", "C++", "TypeScript", "JavaScript"];
+
+// shortenArray<string>(stringArray, 2);
+
